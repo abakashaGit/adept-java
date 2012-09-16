38f00cc8b8f270af9dd05c8d74b7a4f5fbd8fba9
catatonicprime <catatonicprime@gmail.com>
2012-09-16 18:19:53 +0000

    Getting close to PoC.

---
+++ AdeptSdk.java
@@ -0,0 +1,27 @@
+class AdeptSdk {
+	static {
+		//Get the execution path of AdeptSdk, add lib directory.
+		String lib = System.getProperty("user.dir") + "/lib/libdmgrimp.so";
+		try
+		{
+			System.load(lib);
+		}
+		catch(UnsatisfiedLinkError e)
+		{
+			System.out.println("Failed to load C++ Implementation for DMGR:" + lib);
+		}
+	}
+	public static void main(String[] args) {
+		boolean passFail = false;
+		DMGRJavaInterface dmgr = new DMGRJavaInterface();
+		int handle = -1;
+		if(dmgr.DmgrOpen(handle, "Basys2") == true)
+		{
+			System.out.println("success!");
+		}
+		else
+		{
+			System.out.println("fail!");
+		}
+	}
+}
